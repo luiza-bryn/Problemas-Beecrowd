2618e7ce6e1c354163794c7f7acf5b07e6fd4d3b
Luiza Bryn <165062541+luiza-bryn@users.noreply.github.com>
2024-04-20 12:19:27 -0300

    Update magic_and_swords.py

--- magic_and_swords.py
+++ magic_and_swords.py
@@ -1,4 +1,5 @@
 t = int(input("Insira o número de casos testes: "))
+
 magias = ["fire", "water", "earth", "air"]
 dano = [200, 300, 400, 100]
 lv1 = [20, 10, 25, 18]
@@ -13,6 +14,32 @@ dict_lv3 = dict(zip(magias, lv3))
 for i in range(t):
     w, h, x, y = map(int, input().split())
     magia, nivel, cx, cy = input().split()
+    dict_da_vez = None
+    nivel = int(nivel)
+    cx = int(cx)
+    cy = int(cy)
+    
+    if nivel == 1:
+        dict_da_vez = dict_lv1
+    elif nivel == 2:
+        dict_da_vez = dict_lv2
+    elif nivel == 3:
+        dict_da_vez = dict_lv3
 
-    if cx >= x and cx <= w and cy >= y and cy <= h:
-        ...
+    deuDano = False
+    raio = dict_da_vez[magia]
+    dano = dict_dano[magia]
+    
+    if cx <= w and cy <= h:
+        deuDano = True
+    elif cx > w:
+        if (cx - w) <= w:
+            deuDano = True
+    elif cy > h:
+        if (cy - h) <= h:
+            deuDano = True 
+
+    if deuDano:
+        print(dano)
+    else:
+        print(0)
